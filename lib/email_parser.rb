aef908ccdb617bf910281c489da84fea2bb83213
Rick Belson <belsonrj@aol.com>
2020-02-15 23:05:11 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,10 +12,6 @@ class EmailParser
     @csv_emails = csv_emails
   end 
 
-# This will not work because "emails2" below will not be properly parsed  
-# def parse 
-#   csv_emails.split(/\s*,\s*/)
-# end 
 
   def parse 
     csv_emails.split.collect do |address|
