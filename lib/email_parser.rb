91ab15ff3b4695ec8e78f0a37769524e27330765
Rick Belson <belsonrj@aol.com>
2020-02-15 23:04:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -25,11 +25,5 @@ class EmailParser
   end 
 end 
 
-emails1 = "avi@test.com, arel@test.com"
-parser1 = EmailParser.new(emails1)
-parser1.parse 
 
-emails2 = "avi@test.com, arel@test.com"
-parser2 = EmailParser.new(emails2)
-parser2.parse
 
